71958ff1987bfc0996320b2a319b941b861d48cf
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-10-13 11:19:21 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,14 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { Outlet, useLocation } from 'react-router-dom';
-import { getAuth } from 'redux/auth/selectorsAuth';
+// import { getAuth } from 'redux/auth/selectorsAuth';
 
-import { WelcomeUser } from 'components/WelcomeUser/WelcomeUser';
+// import { WelcomeUser } from 'components/WelcomeUser/WelcomeUser';
 import { Navigation } from 'components/Navigation/Navigation';
 import Main from 'pages/Main';
 // import ResponsiveAppBar from 'components/AppBar/AppBar';
 
 export const Layout = () => {
-  const { isAuth } = useSelector(getAuth);
+  // const { isAuth } = useSelector(getAuth);
   const location = useLocation();
   const isHome = location.pathname === '/';
   return (
